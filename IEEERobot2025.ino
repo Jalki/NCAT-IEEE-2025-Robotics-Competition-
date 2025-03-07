1974dbc947977cde4830740a9a32dd07810ac70c
David Nguyen <dvnguyen@aggies.ncat.edu>
2025-03-07 12:47:02 -0500

    Update IEEERobot2025.ino

--- IEEERobot2025.ino
+++ IEEERobot2025.ino
@@ -26,20 +26,12 @@ int previousState;
 
 unsigned long pulsesA = 0;
 
-float positionX = 0, positionY = 0, velocityX = 0, velocityY = 0;
-float accelDriftX = 0, accelDriftY = 0;
-float gyroDriftZ = 0;
-
-#define ACCEL_NOISE_THRESHOLD 0.02  // Adjust based on testing
-#define VELOCITY_DAMPING 0.98  // Reduces velocity gradually over time
-
 int pwm = 255;
 int sec = 1000; //converts milliseconds to seconds in the delay function.
 
 String incomingString = "";
 
 void setup() {
-  Serial.begin(9600);
   Serial1.begin(9600);
   while (!Serial);
   Serial1.println("Started");
@@ -49,7 +41,12 @@ void setup() {
     ard_sensor_check = -1;
     while (1);
   }
-  ard_sensor_check = 1;
+  Serial.print("Gyroscope sample rate = ");
+  Serial.print(IMU.gyroscopeSampleRate());
+  Serial.println(" Hz");
+  Serial.println();
+  Serial.println("Gyroscope in degrees/second");
+  Serial.println("X\tY\tZ");
 
   // initiate (configure) Arduino pins as outputs
   pinMode(D1IN1pin, OUTPUT);
@@ -74,175 +71,260 @@ void setup() {
 }
 
 void loop() {
-  update_position();
+
+  switch(ard_sensor_check)
+  {
+    case -1:
+      Serial.println("Something went seriously wrong? Check your wires and pray its not another faulty Arduino!");
+    case 0:
+      Serial.println("BRO What????");
+    case 1:
+      gyro_pulse();
+      accel_pulse();
+  }
 }
 
-void update_position() {
-  control_movement();
+void gyro_pulse(){
+  float x,y,z;
+  if(IMU.gyroscopeAvailable()){
+    IMU.readGyroscope(x,y,z)
+    ard_sensor_check = 1;
+    Serial1.print(x);
+    Serial1.print('\t');
+    Serial1.print(y);
+    Serial1.print('\t');
+    Serial1.println(z);
+  }
+  else{
+    ard_sensor_check = 0;
+  }
+}
+void accel_pulse(){
+  float x,y,z
+  int degreesX;
+  int degreesY;
+  if(IMU.accelerationAvailable()){
+    IMU.readAcceleration(x,y,z);
+    if(x > 0.1){
+      x = 100 * x;
+      degreesX = map(x, 0, 97, 0, 90);
+      Serial1.print("Tilting up ");
+      Serial1.print(degreesX);
+      Serial1.println(" degrees");
+    }
+    if(x < -0.1){
+      x = 100 * x;
+      degreesX = map(x, 0 , -100, 0, 90);
+      Serial1.print("Tilting down ");
+      Serial1.print(degreesX);
+      Serial1.println(" degrees");
+    }
+    if(y > 0.1){
+      y = 100 * y;
+      degreesY = map(y, 0, 97, 0, 90);
+      Serial1.print("Tilting left ");
+      Serial1.print(degreesY);
+      Serial1.println(" degrees");
+    }
+    if(y < -0.1){
+      y = 100 * y;
+      degreesY = map(y, 0, -100, 0, 90);
+      Serial1.print("Tilting right ");
+      Serial1.print(degreesY);
+      Serial1.print(" degrees");
+    }
+  }
 }
 
-//Directly control the movement of the robot based on uart input from the raspberry pi. 
-void control_movement() {
-  rotate_90_gyro();
+void uart_read(){
+  if (Serial1.available() > 0){
+    //This will read the uart data sent to it
+    int inChar = Serial1.read();
+    if (isDigit(inChar)){
+      incomingString += (char)inChar;
+    }
+    if (inChar == '\n'){
+      Serial.print("Value: ");
+      Serial1.println(incomingString.toInt());
+      Serial1.println(incomingString);
+      incomingString = "";
+    }
+    switch(inChar)
+      case 1:
+        move_forward();
+      case 2:
+        move_backward();
+      case 3:
+        move_Strafe_Right();
+      case 4:
+        move_Strafe_Left();
+      case 5:
+        move_Diagonal_Top_Right();
+      case 6:
+        move_Diagonal_Top_Left();
+      case 7:
+        move_Diagonal_Bottom_Left();
+      case 8:
+        move_Diagonal_Bottom_Right();
+      case 9:
+        move_Rotate_Clockwise();
+      case 10:
+        move_Rotate_CounterClockwise();
+  }
 }
 
+//Moves the robot forward
 //Moves forward
-void move_forward()
+void move_forward() 
 {
-  digitalWrite(D1IN1pin, HIGH);
-  digitalWrite(D1IN2pin, LOW);
-  analogWrite(D1ENA1pin, pwm * 0.75);
+  digitalWrite(D1IN1pin, LOW);
+  digitalWrite(D1IN2pin, HIGH);
+  analogWrite(D1ENA1pin, pwm * 0.50);
 
-  digitalWrite(D1IN3pin, HIGH);
-  digitalWrite(D1IN4pin, LOW);
-  analogWrite(D1ENA2pin, pwm * 0.75);
+  digitalWrite(D1IN3pin, LOW);
+  digitalWrite(D1IN4pin, HIGH);
+  analogWrite(D1ENA2pin, pwm * 0.50);
 
-  digitalWrite(D2IN1pin, HIGH);
-  digitalWrite(D2IN2pin, LOW);
-  analogWrite(D2ENA1pin, pwm * 0.75);
+  digitalWrite(D2IN1pin, LOW);
+  digitalWrite(D2IN2pin, HIGH);
+  analogWrite(D2ENA1pin, pwm * 0.50);
 
-  digitalWrite(D2IN3pin, HIGH);
-  digitalWrite(D2IN4pin, LOW);
-  analogWrite(D2ENA2pin, pwm * 0.75);
+  digitalWrite(D2IN3pin, LOW);
+  digitalWrite(D2IN4pin, HIGH);
+  analogWrite(D2ENA2pin, pwm * 0.50);
 }
 
 //Moves backwards
 void move_backward() 
 {
-  digitalWrite(D1IN1pin, LOW);
-  digitalWrite(D1IN2pin, HIGH);
-  analogWrite(D1ENA1pin, pwm * 0.25);
-
-  digitalWrite(D1IN3pin, LOW);
-  digitalWrite(D1IN4pin, HIGH);
-  analogWrite(D1ENA2pin, pwm * 0.25);
-
-  digitalWrite(D2IN1pin, LOW);
-  digitalWrite(D2IN2pin, HIGH);
-  analogWrite(D2ENA1pin, pwm * 0.25);
-
-  digitalWrite(D2IN3pin, LOW);
-  digitalWrite(D2IN4pin, HIGH);
-  analogWrite(D2ENA2pin, pwm * 0.25);
-}
-
-//This moves the robot to the right via strafe
-void move_Strafe_Right()
-{ 
   digitalWrite(D1IN1pin, HIGH);
   digitalWrite(D1IN2pin, LOW);
-  analogWrite(D1ENA1pin, pwm * 0.25);
-
-  digitalWrite(D1IN3pin, LOW);
-  digitalWrite(D1IN4pin, HIGH);
-  analogWrite(D1ENA2pin, pwm * 0.25);
-
-  digitalWrite(D2IN1pin, LOW);
-  digitalWrite(D2IN2pin, HIGH);
-  analogWrite(D2ENA1pin, pwm * 0.25);
-
-  digitalWrite(D2IN3pin, HIGH);
-  digitalWrite(D2IN4pin, LOW);
-  analogWrite(D2ENA2pin, pwm * 0.25);
-}
-//This moves the robot to the left via strafe
-void move_Strafe_Left()
-{ 
-  digitalWrite(D1IN1pin, LOW);
-  digitalWrite(D1IN2pin, HIGH);
-  analogWrite(D1ENA1pin, pwm * 0.25);
+  analogWrite(D1ENA1pin, pwm * 0.50);
 
   digitalWrite(D1IN3pin, HIGH);
   digitalWrite(D1IN4pin, LOW);
-  analogWrite(D1ENA2pin, pwm * 0.25);
+  analogWrite(D1ENA2pin, pwm * 0.50);
 
   digitalWrite(D2IN1pin, HIGH);
   digitalWrite(D2IN2pin, LOW);
-  analogWrite(D2ENA1pin, pwm * 0.25);
+  analogWrite(D2ENA1pin, pwm * 0.50);
 
-  digitalWrite(D2IN3pin, LOW);
-  digitalWrite(D2IN4pin, HIGH);
-  analogWrite(D2ENA2pin, pwm * 0.25);
+  digitalWrite(D2IN3pin, HIGH);
+  digitalWrite(D2IN4pin, LOW);
+  analogWrite(D2ENA2pin, pwm * 0.50);
 }
-//This moves the robot diagonally top right
-void move_Diagonal_Top_Right()
-{ 
+
+//This moves the robot to the right via strafe
+void move_Strafe_Right(){ 
+  digitalWrite(D1IN1pin, LOW);
+  digitalWrite(D1IN2pin, HIGH);
+  analogWrite(D1ENA1pin, pwm * 0.5);
+
+  digitalWrite(D1IN3pin, HIGH);
+  digitalWrite(D1IN4pin, LOW);
+  analogWrite(D1ENA2pin, pwm * 0.5);
+
+  digitalWrite(D2IN1pin, LOW);
+  digitalWrite(D2IN2pin, HIGH);
+  analogWrite(D2ENA1pin, pwm * 0.5);
+
+  digitalWrite(D2IN3pin, HIGH);
+  digitalWrite(D2IN4pin, LOW);
+  analogWrite(D2ENA2pin, pwm * 0.5);
+}
+//This moves the robot to the left via strafe
+void move_Strafe_Left(){ 
   digitalWrite(D1IN1pin, HIGH);
   digitalWrite(D1IN2pin, LOW);
-  analogWrite(D1ENA1pin, pwm * 0.25);
+  analogWrite(D1ENA1pin, pwm * 0.50);
 
   digitalWrite(D1IN3pin, LOW);
   digitalWrite(D1IN4pin, HIGH);
-  analogWrite(D1ENA2pin, pwm * 0);
+  analogWrite(D1ENA2pin, pwm * 0.50);
+
+  digitalWrite(D2IN1pin, HIGH);
+  digitalWrite(D2IN2pin, LOW);
+  analogWrite(D2ENA1pin, pwm * 0.50);
+
+  digitalWrite(D2IN3pin, LOW);
+  digitalWrite(D2IN4pin, HIGH);
+  analogWrite(D2ENA2pin, pwm * 0.50);
+}
+//This moves the robot diagonally top right
+void move_Diagonal_Top_Right(){
+  digitalWrite(D1IN1pin, HIGH);
+  digitalWrite(D1IN2pin, LOW);
+  analogWrite(D1ENA1pin, pwm * 0);
+
+  digitalWrite(D1IN3pin, LOW);
+  digitalWrite(D1IN4pin, HIGH);
+  analogWrite(D1ENA2pin, pwm * 0.5);
 
   digitalWrite(D2IN1pin, HIGH);
   digitalWrite(D2IN2pin, LOW);
   analogWrite(D2ENA1pin, pwm * 0);
 
-  digitalWrite(D2IN3pin, HIGH);
-  digitalWrite(D2IN4pin, LOW);
-  analogWrite(D2ENA2pin, pwm * 0.25);
+  digitalWrite(D2IN3pin, LOW);
+  digitalWrite(D2IN4pin, HIGH);
+  analogWrite(D2ENA2pin, pwm * 0.5);
 }
 //This moves the robot diagonally top left
-void move_Diagonal_Top_Left()
-{ 
-  digitalWrite(D1IN1pin, HIGH);
-  digitalWrite(D1IN2pin, LOW);
-  analogWrite(D1ENA1pin, pwm * 0);
+void move_Diagonal_Top_Left(){ 
+  digitalWrite(D1IN1pin, LOW);
+  digitalWrite(D1IN2pin, HIGH);
+  analogWrite(D1ENA1pin, pwm * 0.5);
 
   digitalWrite(D1IN3pin, HIGH);
   digitalWrite(D1IN4pin, LOW);
-  analogWrite(D1ENA2pin, pwm * 0.25);
+  analogWrite(D1ENA2pin, pwm * 0);
 
-  digitalWrite(D2IN1pin, HIGH);
-  digitalWrite(D2IN2pin, LOW);
-  analogWrite(D2ENA1pin, pwm * 0.25);
+  digitalWrite(D2IN1pin, LOW);
+  digitalWrite(D2IN2pin, HIGH);
+  analogWrite(D2ENA1pin, pwm * 0.5);
 
   digitalWrite(D2IN3pin, LOW);
   digitalWrite(D2IN4pin, HIGH);
   analogWrite(D2ENA2pin, pwm * 0);
 }
 //This moves the robot diagonally bottom left
-void move_Diagonal_Bottom_Left()
-{
-  digitalWrite(D1IN1pin, LOW);
-  digitalWrite(D1IN2pin, HIGH);
-  analogWrite(D1ENA1pin, pwm * 0.25);
+void move_Diagonal_Bottom_Left(){ 
+  digitalWrite(D1IN1pin, HIGH);
+  digitalWrite(D1IN2pin, LOW);
+  analogWrite(D1ENA1pin, pwm * 0.5);
 
-  digitalWrite(D1IN3pin, LOW);
-  digitalWrite(D1IN4pin, HIGH);
+  digitalWrite(D1IN3pin, HIGH);
+  digitalWrite(D1IN4pin, LOW);
   analogWrite(D1ENA2pin, pwm * 0);
+
+  digitalWrite(D2IN1pin, HIGH);
+  digitalWrite(D2IN2pin, LOW);
+  analogWrite(D2ENA1pin, pwm * 0.5);
+
+  digitalWrite(D2IN3pin, LOW);
+  digitalWrite(D2IN4pin, HIGH);
+  analogWrite(D2ENA2pin, pwm * 0);
+  
+}
+//This moves the robot diagonally bottom right
+void move_Diagonal_Bottom_Right(){ 
+  digitalWrite(D1IN1pin, HIGH);
+  digitalWrite(D1IN2pin, LOW);
+  analogWrite(D1ENA1pin, pwm * 0);
+
+  digitalWrite(D1IN3pin, HIGH);
+  digitalWrite(D1IN4pin, LOW);
+  analogWrite(D1ENA2pin, pwm * 0.5);
 
   digitalWrite(D2IN1pin, HIGH);
   digitalWrite(D2IN2pin, LOW);
   analogWrite(D2ENA1pin, pwm * 0);
 
-  digitalWrite(D2IN3pin, LOW);
-  digitalWrite(D2IN4pin, HIGH);
-  analogWrite(D2ENA2pin, pwm * 0.25);
-  
-}
-//This moves the robot diagonally bottom right
-void move_Diagonal_Bottom_Right()
-{ 
-  digitalWrite(D1IN2pin, HIGH);
-  analogWrite(D1ENA1pin, pwm * 0);
-
-  digitalWrite(D1IN3pin, LOW);
-  digitalWrite(D1IN4pin, HIGH);
-  analogWrite(D1ENA2pin, pwm * 0.25);
-
-  digitalWrite(D2IN1pin, LOW);
-  digitalWrite(D2IN2pin, HIGH);
-  analogWrite(D2ENA1pin, pwm * 0.25);
-
-  digitalWrite(D2IN3pin, LOW);
-  digitalWrite(D2IN4pin, HIGH);
-  analogWrite(D2ENA2pin, pwm * 0);
+  digitalWrite(D2IN3pin, HIGH);
+  digitalWrite(D2IN4pin, LOW);
+  analogWrite(D2ENA2pin, pwm * 0.5);
 }
 //Moves Clockwise
-void move_Rotate_Clockwise()
-{
+void move_Rotate_Clockwise(){
   digitalWrite(D1IN1pin, HIGH);
   digitalWrite(D1IN2pin, LOW);
   analogWrite(D1ENA1pin, pwm * 0.50);
@@ -260,8 +342,7 @@ void move_Rotate_Clockwise()
   analogWrite(D2ENA2pin, pwm * 0.50);
 }
 //Moves Counter Clockwise
-void move_Rotate_CounterClockwise()
-{
+void move_Rotate_CounterClockwise(){  
   digitalWrite(D1IN1pin, LOW);
   digitalWrite(D1IN2pin, HIGH);
   analogWrite(D1ENA1pin, pwm * 0.50);
@@ -280,8 +361,7 @@ void move_Rotate_CounterClockwise()
 }
 
 // Function to stop all motors
-void stop_movement()
-{
+void stop_movement() {
   digitalWrite(D1IN1pin, LOW);
   digitalWrite(D1IN2pin, LOW);
   digitalWrite(D1IN3pin, LOW);
@@ -296,88 +376,31 @@ void stop_movement()
   analogWrite(D2ENA2pin, 0);
 }
 
-void pos_accel(float targetDistance){
-  float ax, ay, az;
-  if (IMU.accelerationAvailable()) {
-    IMU.readAcceleration(ax, ay, az);
-    ax -= accelDriftX;
-    ay -= accelDriftY;
-    // Ignore small values to reduce drift from sensor noise
-    if (abs(ax) < ACCEL_NOISE_THRESHOLD) ax = 0;
-    if (abs(ay) < ACCEL_NOISE_THRESHOLD) ay = 0;
-    // Apply velocity damping before updating position
-    velocityX = velocityX * VELOCITY_DAMPING + ax * 0.02;
-    velocityY = velocityY * VELOCITY_DAMPING + ay * 0.02;
-    velocityX += ax * 0.02;
-    velocityY += ay * 0.02;
-    positionX += velocityX * 0.02;
-    positionY += velocityY * 0.02;
-    Serial.print("Position of the robot: X: ");
-    Serial.print(positionX);
-    Serial.print(" Y: ");
-    Serial.println(positionY);
-  }
-  stop_movement();
+//sweeps from the bottom left of the track. Modify to start from origin
+void sweep(){
+    move_forward();
+    delay(3200);
+    move_Rotate_Clockwise();
+    delay(1290);
+    move_forward();
+    delay(500);
+    move_Rotate_Clockwise();
+    delay(1290);
+    move_forward();
+    delay(3200);
+    move_Rotate_CounterClockwise();
+    delay(1290);
+    move_forward();
+    delay(500);
+    move_Rotate_CounterClockwise();
+    delay(1290);
+    move_forward();
+    delay(3200);
+    stop_movement();
+    delay(10000);
 }
 
-void rotate_gyro(float targetAngle) {
-    float angleRotated = 0;
-    float gx, gy, gz;
-    float gyroDrift = 0;
-    unsigned long prevTime = millis();
-
-    // **1. Measure drift before rotation**  
-    int numSamples = 50;
-    for (int i = 0; i < numSamples; i++) {
-        if (IMU.gyroscopeAvailable()) {
-            IMU.readGyroscope(gx, gy, gz);
-            gyroDrift += gz;  // Accumulate Z-axis drift
-        }
-        delay(10);  // Small delay between samples
-    }
-    gyroDrift /= numSamples;  // Average drift over samples
-
-    // **2. Determine rotation direction**
-    bool clockwise = (targetAngle > 0);
-    if (clockwise) {
-        move_Rotate_Clockwise();
-    } else {
-        move_Rotate_CounterClockwise();
-    }
-
-    // **3. Rotate while compensating for drift**
-    while (abs(angleRotated) <= abs(targetAngle)) {  
-        if (IMU.gyroscopeAvailable()) {
-            IMU.readGyroscope(gx, gy, gz);
-            
-
-            // Apply drift correction
-            gz -= gyroDrift;
-
-            // Compute time elapsed
-            unsigned long currentTime = millis();
-            float deltaTime = (currentTime - prevTime) / 1000.00; // Convert ms to seconds
-            prevTime = currentTime;
-
-            // Integrate gyroscope data to estimate angle rotated
-            angleRotated += gz * deltaTime;
-        }
-    }
-
-    stop_movement();  // Stop once the target angle is reached
-    Serial.print("Rotated");
-    Serial.println(angleRotated);
-}
-
-
-void rotate_90_gyro() {
-    rotate_gyro(90);
-}
-
-void rotate_180_gyro() {
-    rotate_gyro(180);
-}
-
-void rotate_360_gyro() {
-    rotate_gyro(360);
-}
+//Turn on its rear axis
+void move_Turn_RearAxis(){}
+//Turn on its front axis
+void move_Turn_FrontAxis(){}
